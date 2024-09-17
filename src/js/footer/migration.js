92e8fbb1250826714da20587c3567e90a25c1e38
Prince <prince@deriv.com>
2024-09-17 11:04:19 +0400

    chore: migrated all

--- src/js/footer/migration.js
+++ src/js/footer/migration.js
@@ -1,8 +1,48 @@
 
 
-alert('migrated scripts: all')
+// alert('migrated scripts: everything')
 
 window.customLoader = "static version all"
+
+// <!-- Responsive Menu - end of code -->
+// This function adds a red line to the current accordion trigger
+var Webflow = Webflow || [];
+Webflow.push(function () {
+    // Find all accordion triggers
+    let triggers = document.querySelectorAll(".navbar_accordion-trigger");
+
+    triggers.forEach((trigger) => {
+        trigger.addEventListener("click", function () {
+            // Remove the .current class from all triggers
+            triggers.forEach((t) => t.classList.remove("current"));
+
+            // Add the .current class to the clicked trigger
+            this.classList.add("current");
+        });
+    });
+
+    // Find the current page link in Navbar
+    let currentLink = document.querySelector(
+        ".navbar_accordion-link.w--current"
+    );
+
+    if (currentLink) {
+        // Find the closest accordion item
+        let accordionItem = currentLink.closest(".navbar_accordion-item");
+
+        if (accordionItem) {
+            // Find the trigger element within the found parent
+            let trigger = accordionItem.querySelector(".navbar_accordion-trigger");
+
+            if (trigger) {
+                // Add the .current class to the trigger
+                trigger.classList.add("current");
+            }
+        }
+    }
+});
+
+// <!-- Mobile Menu - end of code -->
 
 // <!-- Display current language - start of code -->
 
